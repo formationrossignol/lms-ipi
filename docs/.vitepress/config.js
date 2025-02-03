5a38b71d0ce5894d23a95e49dc6e26c836430d0b
Loïc <92857075+formationrossignol@users.noreply.github.com>
2025-02-03 16:19:49 +0100

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,7 +2,7 @@ export default {
   // site-level options
   title: 'VitePress',
   description: 'Just playing around.',
-  base: './',
+  base: '/vitepress/',
   themeConfig: {
     sidebar: [
       { text: 'Home', link: '/' },

576148625f154328d90540fa6ac2c836f1f29484
Loïc <92857075+formationrossignol@users.noreply.github.com>
2025-02-13 08:29:40 +0100

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,13 +1,68 @@
 export default {
-  // site-level options
+  // Options globales du site
   title: 'VitePress',
-  description: 'Just playing around.',
+  description: 'Une documentation rapide et optimisée.',
   base: '/',
   cleanUrls: true,
+
   themeConfig: {
-    sidebar: [
-      { text: 'Home', link: '/' },
-      { text: 'Presentation', link: '/presentation' }
-    ]
-  }
+    // Navigation principale
+    nav: [
+      { text: 'Accueil', link: '/' },
+      { text: 'Présentation', link: '/presentation' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'Blog', link: '/blog/' },
+      { text: 'Contact', link: '/contact' }
+    ],
+
+    // Barre latérale (Sidebar)
+    sidebar: {
+      '/': [
+        { text: 'Accueil', link: '/' },
+        { text: 'Présentation', link: '/presentation' },
+        { text: 'Guide', link: '/guide/' },
+        { text: 'FAQ', link: '/faq/' }
+      ],
+      '/guide/': [
+        { text: 'Introduction', link: '/guide/' },
+        { text: 'Installation', link: '/guide/installation' },
+        { text: 'Configuration', link: '/guide/configuration' }
+      ]
+    },
+
+    // Recherche intégrée
+    search: {
+      provider: 'local'
+    },
+
+    // Personnalisation du design
+    appearance: true, // Active le mode clair/sombre automatique
+    logo: '/logo.png',
+    siteTitle: 'VitePress Custom',
+
+    // Pied de page
+    footer: {
+      message: 'Publié sous licence MIT.',
+      copyright: '© 2025 VitePress. Tous droits réservés.'
+    },
+
+    // Liens sociaux
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vitepress' },
+      { icon: 'twitter', link: 'https://twitter.com/vitepress' }
+    ],
+
+    // Ajout des breadcrumbs
+    docFooter: {
+      prev: 'Page précédente',
+      next: 'Page suivante'
+    }
+  },
+
+  // SEO et métadonnées
+  head: [
+    ['meta', { name: 'keywords', content: 'vitepress, documentation, guide' }],
+    ['meta', { name: 'author', content: 'Votre Nom' }],
+    ['link', { rel: 'icon', href: '/favicon.ico' }]
+  ]
 }

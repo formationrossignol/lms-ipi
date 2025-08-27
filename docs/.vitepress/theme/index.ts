6d632f3f9fe59568cae9c128d0e739468fca8ccd
Loic Rossignol <formation@rossignol.me>
2025-08-27 08:30:06 +0200

    update

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,16 +1,16 @@
 // .vitepress/theme/index.ts
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
+import ExternalIframe from 'components/ExternalIframe.vue'
 import Reveal from './components/Reveal.vue'
 import SlidesDeck from 'components/SlidesDeck.vue'
-import ExternalIframe from 'components/ExternalIframe.vue'
 import './custom.css'
 
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
+    app.component('ExternalIframe', ExternalIframe),
     app.component('Reveal', Reveal),
     app.component('SlidesDeck', SlidesDeck)
-    app.component('ExternalIframe', ExternalIframe)
   }
 } satisfies Theme

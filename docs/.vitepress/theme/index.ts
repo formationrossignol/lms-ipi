6f27db5ba4de6f4a269816f4f50c7f17284cdfbd
Loic Rossignol <formation@rossignol.me>
2025-08-27 08:17:24 +0200

    update

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,12 +1,14 @@
 // .vitepress/theme/index.ts
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
-import Reveal from '../components/Reveal.vue'
+import Reveal from './components/Reveal.vue'
+import SlidesDeck from './components/SlidesDeck.vue'
 import './custom.css'
 
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
-    app.component('Reveal', Reveal)
+    app.component('Reveal', Reveal),
+    app.component('SlidesDeck', SlidesDeck)
   }
 } satisfies Theme

55eb356e515e7fefb8226623621880d13708bc01
Loic Rossignol <formation@rossignol.me>
2025-08-27 11:32:48 +0200

    update

--- docs/.vitepress/sidebar.ts
+++ docs/.vitepress/sidebar.ts
@@ -2,56 +2,41 @@ export default [
     {
         text: "DevOps Foundation",
         collapsed: true,
-        link: "/cours/cicd"
+        items: [
+            { text: "Le cours", collapsed: true, link: "/cours/cicd" },
+            { text: "Les TP", collapsed: true, link: "/cours/cicd" }
+        ]
     },
     {
         text: "La CI/CD",
         collapsed: true,
         items: [
-            {
-                text: "Le cours", collapsed: true, items: [
-                    { text: "La CICD", link: "/cours/cicd" }
-                ]
-            }
+            { text: "Le cours", collapsed: true, link: "/cours/cicd" },
+            { text: "Les TP", collapsed: true, link: "/cours/cicd" }
         ]
     },
     {
         text: "Docker",
         collapsed: true,
         items: [
-            { text: "Le cours", link: "/pages/categories/les-concepts.md" },
-            {
-                text: "Le cours", collapsed: true, items: [
-                    { text: "Docker", link: "/cours/cicd" }
-                ]
-            }
-
+            { text: "Le cours", collapsed: true, link: "/cours/cicd" },
+            { text: "Les TP", collapsed: true, link: "/cours/cicd" }
         ]
     },
     {
         text: "Terraform",
         collapsed: true,
         items: [
-            { text: "Le cours", link: "/pages/categories/les-concepts.md" },
-            {
-                text: "Le cours", collapsed: true, items: [
-                    { text: "Terraform", link: "/cours/cicd" }
-                ]
-            }
-
+            { text: "Le cours", collapsed: true, link: "/cours/cicd" },
+            { text: "Les TP", collapsed: true, link: "/cours/cicd" }
         ]
     },
     {
         text: "Ansible",
         collapsed: true,
         items: [
-            { text: "Le cours", link: "/pages/categories/les-concepts.md" },
-            {
-                text: "Le cours", collapsed: true, items: [
-                    { text: "Ansible", link: "/cours/cicd" }
-                ]
-            }
-
+            { text: "Le cours", collapsed: true, link: "/cours/cicd" },
+            { text: "Les TP", collapsed: true, link: "/cours/cicd" }
         ]
     }
 ]

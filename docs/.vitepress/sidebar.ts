4df0b9445ba277cae5fabcc0da496db4cbaeef6d
Loic Rossignol <formation@rossignol.me>
2025-08-27 11:13:07 +0200

    update

--- docs/.vitepress/sidebar.ts
+++ docs/.vitepress/sidebar.ts
@@ -2,15 +2,7 @@ export default [
     {
         text: "DevOps Foundation",
         collapsed: true,
-        items: [
-            { text: "Le cours", link: "/pages/categories/les-concepts.md" },
-            {
-                text: "Le cours", collapsed: true, items: [
-                    { text: "DevOps Foundation", link: "/cours/cicd" }
-                ]
-            }
-
-        ]
+        link: "/cours/cicd"
     },
     {
         text: "La CI/CD",
